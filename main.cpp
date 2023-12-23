df766ff943b3157830656d450cbdcf13b309ed05
Ahmed Musharaf <ahmed.musharaf31@gmail.com>
2023-12-23 23:34:37 +0500

    Template

--- main.cpp
+++ main.cpp
@@ -3,9 +3,43 @@
 #include <stack>
 #include <queue>
 #include <algorithm>
+#define ii long long
+
+using namespace std;
+struct Student
+{
+    string name;
+    int studentID;
+    int age;
+    Student *left;
+    Student *right;
+
+    Student(const string &name, int studentID, int age)
+    {
+        this->name = name;
+        this->studentID = studentID;
+        this->age = age;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+class LibrarySystem
+{
+private:
+    vector<Student> arrayBasedList; // Array-based list for storing students
+    Student *linkedListHead;        // Linked list for dynamic student data
+    stack<string> bookStack;        // Stack for book checkout
+    queue<string> bookQueue;        // Queue for book return
+    Student *bstRoot;               // Root of the Binary Search Tree
+
+    public:
+
+};
 
 int main()
 {
-    std::cout << "Hello World..." << std::endl;
+
     return 0;
 }
+
